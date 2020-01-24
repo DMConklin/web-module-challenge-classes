0a8452970a0e7520a88c3669e1339efe87c68ff6
Darrell-Conklin <dmconklin84@gmail.com>
2020-01-23 18:12:32 -0600

    Finished Task 2

--- index.js
+++ index.js
@@ -77,7 +77,27 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
 
+  fill(gallons) {
+    this.tank += gallons;
+  }
+
+  drive(distance) {
+    if ((distance / this.milesPerGallon) > this.tank) {
+      this.odometer += this.tank * this.milesPerGallon;
+      this.tank = 0;
+      return `I ran out of gas at ${this.odometer} miles!`;
+    } else {
+      this.odometer += distance;
+      this.tank -= (distance / this.milesPerGallon);
+    }
+  }
 }
 
 /*
